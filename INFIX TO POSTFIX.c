c317c083d7b746a1f8704ee5507d6dc0c8863d51
Ansh Arora <ansharora2299@gmail.com>
2017-12-19 22:59:43 +0530

    Commit

---
+++ INFIX TO POSTFIX.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+int top=-1,popped,i=0,k=0,j=0;
+char a[20],postfix[20],stack[20];
+void push(char b)
+{
+    top++;
+    stack[top]=b;
+}
+int pop()
+{
+    popped=stack[top];
+    top--;
+    return popped;
+}
+int prec(char op)
+{
+    if(op=='^')
+        return 3;
+    else if(op=='*' || op=='/')
+        return 2;
+    else
+        return 1;
+}
+void infix_postfix(char infix[])
+{
+    for(i=0;infix[i]!='\0';i++)
+    {
+        if(isalnum(infix[i]))
+            postfix[k++]=infix[i];
+        else if(infix[i]=='(')
+            push(infix[i]);
+        else if(infix[i]==')')
+        {
+            while(stack[top]!='(')
+            postfix[k++]=pop();
+            j=pop();
+        }
+        else
+        {
+            if(stack[top]=='(' || top==-1)
+                 push(infix[i]);
+            else if(prec(stack[top])<prec(infix[i]))
+                 push(infix[i]);
+            else
+            {
+                while(top!=-1&&(prec(stack[top])>=prec(infix[i])))
+                postfix[k++]=pop();
+                push(infix[i]);
+            }
+        }
+    }
+    int i;
+    for(i=top;i>=0;i--)
+        postfix[k++]=pop();
+    postfix[k]='\0';
+    printf("\n%s",postfix);
+}
+void main()
+{
+printf("Enter the infix expression\n");
+char a[20];
+scanf("%s",a);
+infix_postfix(a);
+}
