ac9c1f9e260e988bddd06320ca4a7ff872e630bf
Ansh Arora <ansharora2299@gmail.com>
2017-12-22 16:18:35 +0530

    commit

--- CIRCULAR QUEUE.c
+++ CIRCULAR QUEUE.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #define size 5
-int cq[size],f=-1,r=-1,pop,count=0;
+int cq[size],f=-1,r=-1,count=0;
 void insert()
 {
 if(count==size)
@@ -30,7 +30,7 @@ else
 f=(f+1)%size;
 count--;
 }
-return pop;
+return;
 }
 void display()
 {
